4714cab774b547ee6ebc62faaeeca203f8118752
Joakim Granbohm <joakim.granbohm@sysart.fi>
2016-09-27 23:09:40 +0300

    cleanup code

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ import express from 'express';
 const config = require('./config');
 import {getBearerToken, getUserWall, respondFromCache} from './middleware'
 
-const concated = config.twitter.consumer_key + ':' + config.twitter.consumer_secret;
-
 const twitterApiUrl = 'https://api.twitter.com';
 const tokenUrl = twitterApiUrl + '/oauth2/token';
-const timelineUrl = twitterApiUrl + '/1.1/statuses/user_timeline.json?count=10&screen_name=' + config.screen_name
+const timelineUrl = twitterApiUrl + '/1.1/statuses/user_timeline.json?count=10&screen_name=' + config.screen_name;
+
+const concated = config.twitter.consumer_key + ':' + config.twitter.consumer_secret;
 const credentials = new Buffer(concated).toString('base64');
 
 let app = express();
@@ -20,7 +20,7 @@ let getConfig = (req, res, next) => {
         timelineUrl,
         credentials
     };
-    
+
     Object.assign(req, settings);
     next();
 }
