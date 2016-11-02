a04a5bc6be8f4ebd8814002f04b5f8c0e4e08642
Joakim Granbohm <joakim.granbohm@sysart.fi>
2016-11-02 10:31:36 +0200

    change default fetch url arguments (temporary fix)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import {getBearerToken, getUserWall, respondFromCache} from './middleware'
 
 const twitterApiUrl = 'https://api.twitter.com';
 const tokenUrl = twitterApiUrl + '/oauth2/token';
-const timelineUrl = twitterApiUrl + '/1.1/statuses/user_timeline.json?count=10&screen_name=' + config.screen_name;
+const timelineUrl = twitterApiUrl + '/1.1/statuses/user_timeline.json?tweet_mode=extended&count=10&include_rts=false&exclude_replies=true&screen_name=' + config.screen_name;
 
 const concated = config.twitter.consumer_key + ':' + config.twitter.consumer_secret;
 const credentials = new Buffer(concated).toString('base64');
