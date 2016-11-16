cc7daec640a9ce92f745f008d44a0fb38ceb8a6c
Joakim Granbohm <joakim.granbohm@sysart.fi>
2016-11-16 14:47:30 +0200

    add better loggin with file logging support

---
+++ logger.js
@@ -0,0 +1,36 @@
+import chalk from 'chalk';
+import winston from 'winston';
+import fs from 'fs';
+
+const logdir = 'log';
+
+if (!fs.existsSync(logdir)) {
+    fs.mkdirSync(logdir);
+}
+
+const logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)({
+            timestamp() {
+                const now = new Date();
+
+                return `${now.toLocaleDateString('en-GB')} ${now.toLocaleTimeString('en-GB', {hour12: false})}`
+            },
+            formatter(options) {
+                let color;
+                if(options.level === 'info') {
+                    color = 'bgGreen';
+                } else if (options.level === 'error') {
+                    color = 'bgRed';
+                }
+                return `${options.timestamp()} - ${chalk.blue('Tweetserver')} ${chalk[color](options.level)} ${(options.message || '')}`
+            },
+        }),
+
+        new (winston.transports.File)({
+            filename: `${logdir}/server.log`
+        })
+    ]
+});
+
+export default logger;
